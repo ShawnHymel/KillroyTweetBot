7bce5f4f679579811d29b3cf72a2db47f3c3973c
Shawn Hymel <ubuntu@ubuntu.(none)>
2014-04-21 22:08:00 +0000

    Fixed motor control

--- test/motor_test.py
+++ test/motor_test.py
@@ -6,24 +6,45 @@ from struct import *
 # Constants
 INPUT = '0'
 OUTPUT = '1'
+PWM_FREQ = 520
 
 # Pin number
-pin = 12
+dir_pin = 8
 pwm_pin = 5
-pwm_value = 128
+pwm_value = 255
 
 # File locations
-MODE_FILE = '/sys/devices/virtual/misc/gpio/mode/gpio' + str(pin)
-PIN_FILE = '/sys/devices/virtual/misc/gpio/pin/gpio' + str(pin)
+MODE_FILE = '/sys/devices/virtual/misc/gpio/mode/gpio' + str(dir_pin)
+PIN_FILE = '/sys/devices/virtual/misc/gpio/pin/gpio' + str(dir_pin)
 PWM_FILE = '/dev/pwmtimer'
 
 # Pin mode
 os.system('echo ' + OUTPUT + ' > ' + MODE_FILE)
 
-# Set direction
-os.system('echo 1 > ' + PIN_FILE)
-
-# Output PWM to motor
+# Configure PWM
 with open(PWM_FILE, 'wb') as f:
-	pwm_struct = pack('ii', pwm_pin, pwm_value)
-	fcntl.ioctl(f, 0x106, pwm_struct)
+	pwm_struct = pack('iiiI', pwm_pin, 0, 0, PWM_FREQ)
+	fcntl.ioctl(f, 0x107, pwm_struct)
+
+# Motor control
+def drive_motor(dir, pwm):
+	
+	# Set direction
+	os.system('echo ' + str(dir) + ' > ' + PIN_FILE)
+
+	# Output PWM to motor
+	with open(PWM_FILE, 'wb') as f:
+		pwm_struct = pack('ii', pwm_pin, pwm)
+		fcntl.ioctl(f, 0x106, pwm_struct)
+
+# Do stuff with the motor
+drive_motor(0, 0)
+time.sleep(1)
+drive_motor(0, 200)
+time.sleep(2)
+drive_motor(0, 0)
+time.sleep(0.5)
+drive_motor(1, 255)
+time.sleep(2)
+drive_motor(0, 0)
+
--- test/pwm_test.py
+++ test/pwm_test.py
@@ -4,12 +4,13 @@ from struct import *
 
 PWM_FILE = '/dev/pwmtimer'
 
-pwm_pin = 5
-pwm_value = 250
+pwm_pin = 6
+pwm_value = 128
+pwm_freq = 150
 
 # Set frequency
 with open(PWM_FILE, 'wb') as f:
-	pwm_struct = pack('iiiI', pwm_pin, 0, 0, 520)
+	pwm_struct = pack('iiiI', pwm_pin, 0, 0, pwm_freq)
 	fcntl.ioctl(f, 0x107, pwm_struct)
 
 
@@ -18,9 +19,9 @@ with open(PWM_FILE, 'wb') as f:
 	pwm_struct = pack('ii', pwm_pin, pwm_value)
 	fcntl.ioctl(f, 0x106, pwm_struct)
 
-time.sleep(2)
+#time.sleep(2)
 
 # Stop PWM
-with open(PWM_FILE, 'wb') as f:
-	pwm_struct = pack('ii', pwm_pin, 0)
-	fcntl.ioctl(f, 0x106, pwm_struct)
+#with open(PWM_FILE, 'wb') as f:
+#	pwm_struct = pack('ii', pwm_pin, 0)
+#	fcntl.ioctl(f, 0x106, pwm_struct)
