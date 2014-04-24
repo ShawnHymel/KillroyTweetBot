2cf2ba319b08a3f8fddc30ee112dca9340fae740
Shawn Hymel <shawn.hymel@sparkfun.com>
2014-04-24 15:47:58 +0000

    Fixed servo/serial test

--- test/serial_test.py
+++ test/serial_test.py
@@ -1,8 +1,16 @@
 import serial
 
-myPort = serial.Serial('/dev/ttyS1', 9600, timeout=10)
+myPort = serial.Serial('/dev/ttyUSB0', 9600, timeout=10)
 
-data = bytearray.fromhex(u'4A')
+dir = 2            
+
+hex_str = str(hex(dir))
+hex_str = list(hex_str[2:])
+if len(hex_str) < 2:
+	hex_str.insert(0, '0')
+hex_str = unicode("".join(hex_str))
+data = bytearray.fromhex(hex_str)
+#data = bytearray.fromhex(u'0')
 myPort.write(data)
 
 myPort.close()
